a8c31a9bab684e51349c4b7793f8ff59972ce63e
Alekos Filini <alekos.filini@gmail.com>
2017-09-20 16:28:41 +0200

    Completed the bitcoin service

--- app/scripts/services/bitcoin.js
+++ app/scripts/services/bitcoin.js
@@ -13,4 +13,32 @@ angular.module('App')
             return ResourcesGeneratorService.getResource('bitcoin/getinfo').get().$promise
                 .then(ResourcesGeneratorService.successHandler, ResourcesGeneratorService.failureHandler);
         };
+
+        this.getConfirmedBalance = function () {
+            return ResourcesGeneratorService.getResource('bitcoin/getconfirmedbalance').get().$promise
+                .then(ResourcesGeneratorService.successHandler, ResourcesGeneratorService.failureHandler);
+        };
+
+        this.getUnconfirmedBalance = function () {
+            return ResourcesGeneratorService.getResource('bitcoin/getunconfirmedbalance').get().$promise
+                .then(ResourcesGeneratorService.successHandler, ResourcesGeneratorService.failureHandler);
+        };
+
+        this.getNewAddress = function () {
+            return ResourcesGeneratorService.getResource('bitcoin/getnewaddress').get().$promise
+                .then(ResourcesGeneratorService.successHandler, ResourcesGeneratorService.failureHandler);
+        };
+
+        this.sendToAddress = function (address, amount) {
+            return ResourcesGeneratorService.getResource('bitcoin/sendtoaddress').post({
+                address: address,
+                amount: amount
+            }).$promise
+                .then(ResourcesGeneratorService.successHandler, ResourcesGeneratorService.failureHandler);
+        };
+
+        this.getRawTransaction = function (txid) {
+            return ResourcesGeneratorService.getResource('bitcoin/getrawtransaction/:txid').get({txid: txid}).$promise
+                .then(ResourcesGeneratorService.successHandler, ResourcesGeneratorService.failureHandler);
+        };
     });
