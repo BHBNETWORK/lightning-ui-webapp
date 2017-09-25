1d7251f969136e810e3d681c703dd74529a8a5f5
Alekos Filini <alekos.filini@gmail.com>
2017-09-25 08:54:58 +0200

    Add the form to allocate funds for Lightning

--- app/scripts/controllers/wallet.js
+++ app/scripts/controllers/wallet.js
@@ -8,8 +8,60 @@
  * Controller of the App
  */
 angular.module('App')
-    .controller('WalletCtrl', function ($scope, $rootScope, $location) {
+    .controller('WalletCtrl', function ($scope, $rootScope, $location, $q, BitcoinService, LightningService, $mdToast) {
         $rootScope.$emit('page-title', 'Wallet');
+
+        $scope.addfunds = {
+            amount: 0,
+            autorawtx: true,
+            rawtx: '',
+            creatingTx: false
+        };
+
+        function resetAddFunds() {
+            $scope.addfunds.amount = 0;
+            $scope.addfunds.autorawtx = false;
+            $scope.addfunds.rawtx = '';
+            $scope.addfunds.creatingTx = false;
+        }
+
+        $scope.confirmAddFunds = function () {
+            // TODO: alert(?)
+
+            var rawTxPromise = $q.resolve({rawtx: $scope.addfunds.rawtx});
+            if (!$scope.addfunds.rawtx) {
+                $scope.addfunds.creatingTx = true;
+
+                rawTxPromise = LightningService.getNewAddress()
+                    .then(function (addr) {
+                        return BitcoinService.sendToAddress(addr.address, ($scope.addfunds.amount / 1e8).toFixed(8));
+                    })
+                    .then(function (txid) {
+                        return BitcoinService.getRawTransaction(txid.txid);
+                    })
+                    .then(function (rawtx) {
+                        $scope.addfunds.creatingTx = false;
+                        return rawtx;
+                    });
+            }
+
+            rawTxPromise
+                .then(function (rawtx) {
+                    return LightningService.addFunds(rawtx.rawtx);
+                })
+                .then(function (response) {
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .textContent('Added ' + response.satoshis + ' SAT to your Lightning wallet')
+                            .highlightAction(true)
+                            .position('bottom right')
+                            .hideDelay(false)
+                            .action('Close')
+                    );
+
+                    resetAddFunds();
+                });
+        };
 
         $scope.goTo = function (path) {
             $location.path(path);
