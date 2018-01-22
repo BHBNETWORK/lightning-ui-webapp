68e313043a2eac4a0e53e68d53ce96aa3dc36ff8
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-01-22 17:50:34 +0100

    stress_curl_test.sh

---
+++ stress_curl_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for i in `seq 1 10`;
+do
+    echo "Test n. " $i
+
+    curl http://127.0.0.1:9000/api/lightning/listinvoice &
+    curl http://127.0.0.1:9000/api/lightning/listpeers &
+    curl http://127.0.0.1:9000/api/lightning/getnewaddress &
+    curl http://127.0.0.1:9000/api/lightning/listfunds &
+    curl http://127.0.0.1:9000/api/lightning/listinvoices &
+    curl http://127.0.0.1:9000/api/lightning/listpeers &
+done
