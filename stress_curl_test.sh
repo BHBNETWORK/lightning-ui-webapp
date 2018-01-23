ba3bab0a5d77968d9594fae77c0f26b79a193fa5
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-01-23 18:35:56 +0100

    stress_curl_test.sh: fix typo on api call

--- stress_curl_test.sh
+++ stress_curl_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for i in `seq 1 10`;
+for i in `seq 1 2`;
 do
     echo "Test n. " $i
 
-    curl http://127.0.0.1:9000/api/lightning/listinvoice &
+    curl http://127.0.0.1:9000/api/lightning/listinvoices &
     curl http://127.0.0.1:9000/api/lightning/listpeers &
     curl http://127.0.0.1:9000/api/lightning/getnewaddress &
     curl http://127.0.0.1:9000/api/lightning/listfunds &
