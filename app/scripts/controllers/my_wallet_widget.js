a4d58b54782849b0ff754bb1e4908490e827af1c
Alekos Filini <alekos.filini@gmail.com>
2018-03-14 16:28:36 +0100

    Remove the Bitcoin confirmed and unconfirmed balance

--- app/scripts/controllers/my_wallet_widget.js
+++ app/scripts/controllers/my_wallet_widget.js
@@ -8,7 +8,7 @@
  * Controller of the App
  */
 angular.module('App')
-    .controller('MyWalletWidgetCtrl', function ($q, $interval, BitcoinService, LightningService) {
+    .controller('MyWalletWidgetCtrl', function ($q, $interval, LightningService) {
         var _self = this;
 
         var UPDATE_DELAY = 120 * 1000;
@@ -18,47 +18,31 @@ angular.module('App')
         this.lastUpdate = null;
 
         this.lightningAddress = '';
-        this.bitcoinAddress = '';
-        this.confirmedBalance = 0;
-        this.unconfirmedBalance = 0;
         this.lightningFunds = 0;
 
-        this.updateAddresses = function (){
-            const updateAddress = function (address, service){
-                if ('undefined' === typeof (window.localStorage [address])){
-                    const promises = [service.getNewAddress()];
-                    $q.all(promises)
-                        .then(function (response) {
-                            window.localStorage [address] = _self [address] = response[0].address;
-                        })
-                        .catch(function (err) {
-                            console.log (JSON.stringify (err));
-                        });
-                }
-                else{
-                    _self [address] = window.localStorage [address];
-                }
-            };
-            updateAddress ('bitcoinAddress', BitcoinService);
-            updateAddress ('lightningAddress', LightningService);
+        this.updateAddress = function () {
+            if ('undefined' === typeof (window.localStorage.lightningAddress)) {
+                LightningService.getNewAddress()
+                    .then(function (response) {
+                        window.localStorage.lightningAddress = _self.lightningAddress = response.address;
+                    })
+                    .catch(function (err) {
+                        console.log(JSON.stringify(err));
+                    });
+            }
+            else {
+                _self.lightningAddress = window.localStorage.lightningAddress;
+            }
         };
 
         this.update = function () {
             _self.loading = true;
 
-            const promises = [
-                BitcoinService.getConfirmedBalance(),
-                BitcoinService.getUnconfirmedBalance(),
-                LightningService.getFundsSum()
-            ];
+            _self.updateAddress();
 
-            _self.updateAddresses ();
-
-            $q.all(promises)
+            LightningService.getFundsSum()
                 .then(function (response) {
-                    _self.confirmedBalance = response[0].balance * 1e8;
-                    _self.unconfirmedBalance = response[1].balance * 1e8;
-                    _self.lightningFunds = response[2];
+                    _self.lightningFunds = response;
 
                     _self.lastUpdate = new Date();
                     _self.loading = false;
@@ -69,26 +53,14 @@ angular.module('App')
                 });
         };
 
-        this.newAddress = function (address, service){
-            const promises = [
-                service.getNewAddress()
-            ];
-
-            $q.all(promises)
+        this.newLightningAddress = function () {
+            LightningService.getNewAddress()
                 .then(function (response) {
-                    window.localStorage [address] = _self [address] = response[0].address;
+                    window.localStorage.lightningAddress = _self.lightningAddress = response.address;
                 })
                 .catch(function (err) {
                     console.log (JSON.stringify (err));
                 });
-        };
-
-        this.newBitcoinAddress = function(){
-            _self.newAddress ('bitcoinAddress', BitcoinService);
-        };
-
-        this.newLightningAddress = function(){
-            _self.newAddress ('lightningAddress', LightningService);
         };
 
         this.update();
